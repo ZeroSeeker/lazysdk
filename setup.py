444183f6c0d795ef011f6ab680f2935baeee3196
ZeroSeeker <zeroseeker@foxmail.com>
2022-06-07 10:44:33 +0800

    增加lazyredis模块

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="lazysdk",
-    version="0.0.9",
+    version="0.0.10",
     description="基于Python的懒人包",
     # long_description=long_description,
     # long_description_content_type="text/markdown",
@@ -30,6 +30,7 @@ setuptools.setup(
         'showlog==0.0.6',
         'requests==2.27.1',
         'envx==0.0.5',
-        'pytz==2021.3'
+        'pytz==2021.3',
+        'redis==4.1.0'
     ]
 )
