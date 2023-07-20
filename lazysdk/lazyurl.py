bcffa9c915b52b519e468e4ecbb7ffb428995391
ZeroSeeker <1260812287@qq.com>
2023-07-20 15:37:41 +0800

    修复bug

--- lazysdk/lazyurl.py
+++ lazysdk/lazyurl.py
@@ -8,12 +8,15 @@ def get_url_params(
     获取url的params参数，返回dict形式
     """
     params_str = urlparse.urlsplit(url).query
-    params_str_split = params_str.split('&')
-    params_dict = dict()
-    for each in params_str_split:
-        each_split = each.split('=', maxsplit=1)
-        params_dict[each_split[0]] = each_split[1]
-    return params_dict
+    if params_str:
+        params_str_split = params_str.split('&')
+        params_dict = dict()
+        for each in params_str_split:
+            each_split = each.split('=', maxsplit=1)
+            params_dict[each_split[0]] = each_split[1]
+        return params_dict
+    else:
+        return
 
 
 def url_info(
