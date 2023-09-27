0400652f8d2b855c692fdefa13270408c927b399
ZeroSeeker <1260812287@qq.com>
2023-09-27 18:36:17 +0800

    精简依赖

--- lazysdk/__init__.py
+++ lazysdk/__init__.py
@@ -6,17 +6,4 @@
 @ GitHub : https://github.com/ZeroSeeker
 @ Gitee : https://gitee.com/ZeroSeeker
 """
-from . import lazyprocess
-from . import lazyrequests
-from . import lazybase64
-from . import lazytime
-from . import lazywebhook
-from . import lazyrandom
-from . import lazyip
-from . import lazyflask
-from . import lazyid
-from . import lazyurl
-from . import lazyua
-from . import lazy_id_card
-from . import showdata
 
