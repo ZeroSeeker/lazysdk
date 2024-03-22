a4a899dcd4eb585e9950e28f0056bd6292f4c99d
ZeroSeeker <1260812287@qq.com>
2024-03-22 14:25:54 +0800

    增加xml处理模块

--- lazysdk/lazytext.py
+++ lazysdk/lazytext.py
@@ -75,3 +75,25 @@ def file_name_format(text):
     """
     text_new = re.sub(r'[\\/:*?"<>|\r\n]+', "_", text)  # 规避windows非法字符
     return text_new
+
+
+def name_head_list(
+        your_name: str
+):
+    """
+    获取姓名的首字母缩写的所有可能列表
+    :param your_name:
+    :return:
+    """
+    from xpinyin import Pinyin
+    p = Pinyin()
+    name_pinyins = p.get_pinyins(your_name)
+    name_heads = list()
+    for each_name_pinyin in name_pinyins:
+        name_pinyin_split = each_name_pinyin.split('-')
+        name_head = ""
+        for each_split in name_pinyin_split:
+            each_split_0 = each_split[0]
+            name_head += each_split_0
+        name_heads.append(name_head)
+    return name_heads
